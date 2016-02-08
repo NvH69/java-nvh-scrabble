6a6dbbc837f0f79f934d6ce2571f21034ac4d1a1
Nicolas Vanhauter <nvhfr@yahoo.fr>
2016-02-08 10:49:44 +0100

    feat(design): Changement de position de la barre de progression

--- src/main/java/com/nvh/scrabble/view/internalwindows/ProgressWindow.java
+++ src/main/java/com/nvh/scrabble/view/internalwindows/ProgressWindow.java
@@ -12,26 +12,23 @@ import java.util.Observer;
 
 public class ProgressWindow extends JFrame implements Observer {
     private static final long serialVersionUID = -596843077674913400L;
-    static public JProgressBar progressBar = new JProgressBar();
+    static public JProgressBar progressBar = new JProgressBar(JProgressBar.VERTICAL, 0, Dictionary.dictionary.size());
 
     public ProgressWindow() {
         this.setFont(new Font(MainWindow.mainFont, Font.PLAIN, 11));
 
-        this.setTitle("Recherche...");
-        this.setBounds(600, 460, 276, 64);
+        this.setBounds(168, 150, 45, 620);
         this.setAlwaysOnTop(true);
+        this.setUndecorated(true);
         JPanel contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
         this.setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        progressBar.setMinimum(0);
-        progressBar.setMaximum(Dictionary.dictionary.size() - 30000);
-
         progressBar.setFont(new Font(MainWindow.mainFont, Font.PLAIN, 12));
-        progressBar.setBounds(0, 0, 270, 35);
-        progressBar.setStringPainted(true);
+        progressBar.setBounds(0, 0, 45, 620);
+        progressBar.setBackground(Color.black);
         contentPane.add(progressBar, BorderLayout.CENTER);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
